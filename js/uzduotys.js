872e8b481eb1a4bfeae8edd6b7d9ce71c02fc456
Tomas Karciauskas <tikis555@outlook.com>
2020-04-01 22:11:09 +0300

    css animacijos, js uzduotis

---
+++ js/uzduotys.js
@@ -0,0 +1,16 @@
+[a = -5, b = 0];
+[c = -1, d = 4];
+let x;
+let y;
+let z;
+if (a <= c) {
+// x = a - (a+c);
+// y = b - (b % d);
+// z = b - y;
+    x = (b - d);
+    y = (a - c);
+
+}
+console.log(x, y);
+//TODO pabaigti uzduoti (rasti sankirta pvz
+// x= -4 ir y= 0
