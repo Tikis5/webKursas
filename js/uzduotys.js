1d1336e085e7fba42a1e6b63500875d3874d5dbc
Tomas Karciauskas <tikis555@outlook.com>
2020-04-02 19:04:37 +0300

    js bandymas html kode

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -1,16 +1,9 @@
 [a = -5, b = 0];
-[c = -1, d = 4];
+[c = -4, d = 4];
 let x;
 let y;
-let z;
 if (a <= c) {
-// x = a - (a+c);
-// y = b - (b % d);
-// z = b - y;
-    x = (b - d);
-    y = (a - c);
-
+    x = c;
+    y = b;
 }
 console.log(x, y);
-//TODO pabaigti uzduoti (rasti sankirta pvz
-// x= -4 ir y= 0
