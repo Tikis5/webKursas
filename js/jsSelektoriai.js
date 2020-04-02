e5f3096e9a7f5c37c824ee308ca78c76af0faf74
Tomas Karciauskas <tikis555@outlook.com>
2020-04-02 21:34:54 +0300

    js selektoriai, uzduotys

---
+++ js/jsSelektoriai.js
@@ -0,0 +1,11 @@
+let xyz = document.getElementById('unikalus_elementas');
+let zyx = document.getElementsByClassName('galerijos_foto')[0];
+let abc = document.querySelectorAll('.kita .kita_klase')[2];
+xyz.innerHTML = 'Labas pasauli!';
+zyx.src = '../img/lempute_off.png';
+abc.style.backgroundColor = 'red';
+abc.style.display = 'block';
+
+console.log(xyz.innerHTML);
+console.log(zyx.src);
+console.log(abc.style.display);
---
+++ js/uzduotis.js
@@ -0,0 +1,41 @@
+let foto = document.getElementsByTagName('img');
+/*1*/
+console.log('Puslapyje paveiksleliu yra: ' + foto.length);
+
+/*2*/
+xyz.innerHTML = 'Puslapyje paveiksleliu yra: ' + foto.length;
+
+/*3 uzduotis html kode*/
+//onclick = "window.alert('Puslapyje paveiksleliu yra: ' + foto.length)";
+
+/*4*/
+let fotos = document.querySelectorAll('img:nth-child(2n)')
+
+for (let i = 0; i < fotos.length; i++) {
+    elem = fotos[i];
+    elem.setAttribute("alt", "Testas");
+}
+
+/*5*/
+let style = {
+    "font-size": "24px",
+    "background-color": "green",
+    "width": "200px",
+    "height": "200px"
+};
+//Pirmas budas
+// xyz.style.fontSize = '24px';
+// xyz.style.backgroundColor = 'green';
+// xyz.style.width = '200px';
+// xyz.style.height = '200px';
+
+//Antras budas
+// for (let key in style){
+//     xyz.style[key] = style[key]
+// }
+
+//Trecias budas
+// Object.assign(xyz.style, style);
+
+//Ketvirtas budas
+xyz.setAttribute("class", "stilius2");
